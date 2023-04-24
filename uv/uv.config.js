8cb88b683c6b4a0671e27c3bca0f801eb30938fe
Parrot <knowing.discos0e@icloud.com>
2023-04-24 16:07:47 +0000

    aaa

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://artclass-production-40e0.up.railway.app/bare/',
     encodeUrl: Ultraviolet.codec.base64.encode,
     decodeUrl: Ultraviolet.codec.base64.decode,
     handler: '/uv/uv.handler.js',
